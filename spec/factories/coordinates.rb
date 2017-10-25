f7f7ae8b472276522c6607eb5b8961986e00eba7
Edgars Tubelis <edgars.tubelis@columba.de>
2017-10-25 18:21:37 +0200

    added sample test

--- spec/factories/coordinates.rb
+++ spec/factories/coordinates.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     street_address { Faker::Address.street_address }
   end
 
-  factory invalid_address do
+  factory :invalid_address do
     street_address {'bla-blup'}
   end
 end
--- spec/requests/coordinates_spec.rb
+++ spec/requests/coordinates_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Coordinates API', type: :request do
+
   describe 'GET /coordinates/:address' do
-    
+    context 'for valid address' do
+      let(:address) { :valid_address }
+
+      before { get "/coordinates/#{:address}" }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
   end
 end
